cf8f6a2f0903d026f3798e3ed9c2960f5d35842b
IvanManhattan <IvanManhattan>
2024-02-27 18:12:46 +0300

    Pleifer encryption

---
+++ Lab1/Lab1/Lab1/MainForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Lab1 {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RadioPanel = new System.Windows.Forms.Panel();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.RadioPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(463, 92);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 150);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(463, 297);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(300, 150);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(463, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Encrypted text";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(463, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Decrypted text";
+            // 
+            // RadioPanel
+            // 
+            this.RadioPanel.Controls.Add(this.radioButton2);
+            this.RadioPanel.Controls.Add(this.radioButton1);
+            this.RadioPanel.Location = new System.Drawing.Point(46, 142);
+            this.RadioPanel.Name = "RadioPanel";
+            this.RadioPanel.Size = new System.Drawing.Size(200, 100);
+            this.RadioPanel.TabIndex = 4;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Location = new System.Drawing.Point(16, 55);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(115, 30);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Location = new System.Drawing.Point(16, 25);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(115, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 52);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RadioPanel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Encryptor";
+            this.RadioPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.Panel RadioPanel;
+        private System.Windows.Forms.RadioButton radioButton2;
+
+        private System.Windows.Forms.RadioButton radioButton1;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.TextBox textBox2;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        #endregion
+    }
+}
---
+++ Lab1/Lab1/Lab1/MainForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab1 {
+    
+    public partial class MainForm : Form {
+
+        private char[,] _encryptTable = {
+            {'C', 'R', 'Y', 'P', 'T'},
+            {'O', 'G', 'A', 'H', 'B'},
+            {'D', 'E', 'F', 'I', 'K'},
+            {'L', 'M', 'N', 'Q', 'S'},
+            {'U', 'V', 'W', 'X', 'Z'}
+        };
+
+        public static char VoidSymbol = 'X';
+        
+        private string _textToEncrypt; 
+        
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        private char[] prepareTextPleifer() {
+            
+            for (int i = 0; i < _textToEncrypt.Length - 1; i++) {
+                if (_textToEncrypt[i] == _textToEncrypt[i + 1]) {
+                    _textToEncrypt.Insert(i + 1, new string(VoidSymbol, 1));
+                }
+            }
+
+            if ((_textToEncrypt.Length & 1) != 0) {
+                _textToEncrypt += VoidSymbol;
+            }
+
+            var textToEncryptArray = new char[_textToEncrypt.Length];
+            
+            for (int j = 0; j < _textToEncrypt.Length; j++) {
+                textToEncryptArray[j] = _textToEncrypt[j];
+            }
+
+            return textToEncryptArray;
+
+        }
+        
+        private string EncryptText() {
+            _textToEncrypt = textBox1.Text;
+            
+            int j = 0;
+            int action = 0;
+            int[,] indexes = new int[2,2];
+            char[] symbols = new char[2];
+            int k = 0;
+            
+            var textToEncryptArray = prepareTextPleifer();
+            var encryptedText = new char[textToEncryptArray.Length];
+            
+            for (int i = 0; i < textToEncryptArray.Length; i += 2) {
+                symbols[k] = _textToEncrypt[i];
+                symbols[k + 1] = _textToEncrypt[i + 1];
+
+                indexes = GetIndex(symbols);
+                action = CheckTable(indexes);
+
+                if (action == 1) {
+                    if (indexes[0, 1] + 1 >= _encryptTable.GetLength(0)) {
+                        encryptedText[i] = _encryptTable[indexes[0, 0], 0];
+                        encryptedText[i + 1] = _encryptTable[indexes[1, 0], indexes[1, 1] + 1];
+                    }
+                    else if (indexes[1, 1] + 1 >= _encryptTable.GetLength(0)) {
+                        encryptedText[i] = _encryptTable[indexes[0, 0], indexes[0, 1] + 1];
+                        encryptedText[i + 1] = _encryptTable[indexes[1, 0], 0];
+                    }
+                    else {
+                        encryptedText[i] = _encryptTable[indexes[0, 0], indexes[0, 1] + 1];
+                        encryptedText[i + 1] = _encryptTable[indexes[1, 0], indexes[1, 1] + 1];
+                    }
+                }
+
+                if (action == 2) {
+                    if (indexes[0, 0] + 1 >= _encryptTable.GetLength(0)) {
+                        encryptedText[i] = _encryptTable[0, indexes[0, 1]];
+                        encryptedText[i + 1] = _encryptTable[indexes[1, 0] + 1, indexes[1, 1]];
+                    }
+                    else if (indexes[1, 0] + 1 >= _encryptTable.GetLength(0)) {
+                        encryptedText[i] = _encryptTable[indexes[0, 0] + 1, indexes[0, 1]];
+                        encryptedText[i + 1] = _encryptTable[0, indexes[1, 1]];
+                    }
+                    else {
+                        encryptedText[i] = _encryptTable[indexes[0, 0] + 1, indexes[0, 1]];
+                        encryptedText[i + 1] = _encryptTable[indexes[1, 0] + 1, indexes[1, 1]];
+                    }
+                }
+
+                if (action == 3) {
+                    int delta = Math.Abs(indexes[0, 1] - indexes[1, 1]);
+                    if (indexes[0, 1] < indexes[1, 1]) {
+                        encryptedText[i] = _encryptTable[indexes[0, 0], indexes[0, 1] + delta];
+                        encryptedText[i + 1] = _encryptTable[indexes[1, 0], indexes[1, 1] - delta];
+                    }
+                    else {
+                        encryptedText[i] = _encryptTable[indexes[0, 0], indexes[0, 1] - delta];
+                        encryptedText[i + 1] = _encryptTable[indexes[1, 0], indexes[1, 1] + delta];
+                    }
+                }
+
+                if (action == 4) {
+                    continue;
+                }
+            }
+
+            return new string(encryptedText);
+        }
+
+        private int[,] GetIndex(char[] symbols) {
+            int[,] indexes = new int[2,2];
+            bool isFound = false;
+            
+            for (int k = 0; k < 2; k++) {
+                for (int i = 0; i < _encryptTable.GetLength(0); i++) {
+                    for (int j = 0; j < _encryptTable.GetLength(0); j++) {
+                        if (symbols[k] == _encryptTable[i, j]) {
+                            isFound = true;
+                            indexes[k, 0] = i;
+                            indexes[k, 1] = j;
+                            break;
+                        }
+                    }
+                    if (isFound) {
+                        isFound = false;
+                        break;
+                    }
+                }
+            }
+
+            return indexes;
+        }
+
+        private int CheckTable(int[,] indexes) {
+
+            if (indexes[0, 0] == indexes[1, 0]) {
+                return 1;
+            }
+            if (indexes[0, 1] == indexes[1, 1]) {
+                return 2;
+            }
+
+            if (indexes[0, 0] != indexes[1, 0] && indexes[0, 1] != indexes[1, 1]) {
+                return 3;
+            }
+            
+            if (indexes[0, 0] == indexes[1, 0] && indexes[0, 1] == indexes[1, 1]) {
+                return 4;
+            }
+
+            return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            textBox2.Text = EncryptText();
+        }
+    }
+}
